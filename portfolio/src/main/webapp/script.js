2c0834ace20657713be34410a7633a640b294b03
solmos1900 <sebastian-olmos@uiowa.edu>
2021-03-05 23:05:42 +0000

    assign the textFromResponse variable to the greetingContainer

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,14 +17,14 @@
  */
 async function addRandomGreeting() {
   const responseFromServer = await fetch('/hello');
-  const textFromResponse = await responseFromServer.text();
-  const greetings =
+  const textFromResponse = await responseFromServer.json();
+  /*const greetings =
       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
   // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  const greeting = greetings[Math.floor(Math.random() * greetings.length)];*/
 
   // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  greetingContainer.innerText = textFromResponse;
 }
